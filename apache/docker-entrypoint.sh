8831c23c4104d9335d26058f5737a11e148467d2
Taras Machyshyn <taras@espocrm.com>
2021-10-06 18:51:15 +0300

    v 7.0.1

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -2,12 +2,169 @@
 
 set -euo pipefail
 
+# entrypoint-utils.sh
+configPrefix="ESPOCRM_CONFIG_"
+
+compareVersion() {
+    local version1="$1"
+    local version2="$2"
+    local operator="$3"
+
+    echo $(php -r "echo version_compare('$version1', '$version2', '$operator');")
+}
+
+join() {
+    local sep="$1"; shift
+    local out; printf -v out "${sep//%/%%}%s" "$@"
+    echo "${out#$sep}"
+}
+
+getConfigParamFromFile() {
+    local name="$1"
+
+    php -r "
+        if (file_exists('$DOCUMENT_ROOT/data/config-internal.php')) {
+            \$config=include('$DOCUMENT_ROOT/data/config-internal.php');
+
+            if (array_key_exists('$name', \$config)) {
+                die(\$config['$name']);
+            }
+        }
+
+        \$config=include('$DOCUMENT_ROOT/data/config.php');
+        if (array_key_exists('$name', \$config)) {
+            die(\$config['$name']);
+        }
+    "
+}
+
+getConfigParam() {
+    local name="$1"
+
+    php -r "
+        require_once('$DOCUMENT_ROOT/bootstrap.php');
+
+        \$app = new \Espo\Core\Application();
+        \$config = \$app->getContainer()->get('config');
+
+        echo \$config->get('$name');
+    "
+}
+
+# Bool: saveConfigParam "jobRunInParallel" "true"
+# String: saveConfigParam "language" "'en_US'"
+saveConfigParam() {
+    local name="$1"
+    local value="$2"
+
+    php -r "
+        require_once('$DOCUMENT_ROOT/bootstrap.php');
+
+        \$app = new \Espo\Core\Application();
+        \$config = \$app->getContainer()->get('config');
+
+        if (\$config->get('$name') !== $value) {
+            \$config->set('$name', $value);
+            \$config->save();
+        }
+    "
+}
+
+applyConfigEnvironments() {
+    local envName
+    local envValue
+    local configParamName
+    local configParamValue
+
+    compgen -v | while read -r envName; do
+
+        if [[ $envName == "$configPrefix"* ]]; then
+
+            envValue="${!envName}"
+
+            configParamName=$(normalizeConfigParamName "$envName")
+            configParamValue=$(normalizeConfigParamValue "$envValue")
+
+            saveConfigParam "$configParamName" "$configParamValue"
+
+        fi
+
+    done
+}
+
+isQuoteValue() {
+    local value="$1"
+
+    php -r "
+        echo isQuote('$value');
+
+        function isQuote (\$value) {
+            \$value = trim(\$value);
+
+            if (\$value === '0') {
+                return false;
+            }
+
+            if (empty(\$value)) {
+                return true;
+            }
+
+            if (!preg_match('/[^0-9.]+/', \$value)) {
+                return false;
+            }
+
+            if (in_array(\$value, ['null', '1'], true)) {
+                return false;
+            }
+
+            if (in_array(\$value, ['true', 'false'])) {
+                return false;
+            }
+
+            return true;
+        }
+    "
+}
+
+normalizeConfigParamName() {
+    local value="$1"
+
+    php -r "
+        \$value = str_ireplace('$configPrefix', '', '$value');
+        \$value = strtolower(\$value);
+
+        \$value = preg_replace_callback(
+            '/_([a-zA-Z])/',
+            function (\$matches) {
+                return strtoupper(\$matches[1]);
+            },
+            \$value
+        );
+
+        echo \$value;
+    "
+}
+
+normalizeConfigParamValue() {
+    local value=${1//\'/\\\'}
+
+    local isQuoteValue=$(isQuoteValue "$value")
+
+    if [ -n "$isQuoteValue" ] && [ "$isQuoteValue" = 1 ]; then
+        echo "'$value'"
+        return
+    fi
+
+    echo "$value"
+}
+# END: entrypoint-utils.sh
+
 installationType() {
     if [ -f "$DOCUMENT_ROOT/data/config.php" ]; then
-        local isInstalled=$(php -r "\$config=include('$DOCUMENT_ROOT/data/config.php'); echo \$config['isInstalled'];")
+        local isInstalled=$(getConfigParamFromFile "isInstalled")
 
         if [ -n "$isInstalled" ] && [ "$isInstalled" = 1 ]; then
-            local installedVersion=$(php -r "\$config=include('$DOCUMENT_ROOT/data/config.php'); echo \$config['version'];")
+            local installedVersion=$(getConfigParamFromFile "version")
             local isVersionGreater=$(compareVersion "$ESPOCRM_VERSION" "$installedVersion" ">")
 
             if [ -n "$isVersionGreater" ]; then
@@ -24,20 +181,6 @@ installationType() {
     fi
 
     echo "install"
-}
-
-compareVersion() {
-    local version1="$1"
-    local version2="$2"
-    local operator="$3"
-
-    echo $(php -r "echo version_compare('$version1', '$version2', '$operator');")
-}
-
-join() {
-    local sep="$1"; shift
-    local out; printf -v out "${sep//%/%%}%s" "$@"
-    echo "${out#$sep}"
 }
 
 actionInstall() {
@@ -67,7 +210,7 @@ actionUpgrade() {
         return
     fi
 
-    local installedVersion=$(php -r "\$config=include('$DOCUMENT_ROOT/data/config.php'); echo \$config['version'];")
+    local installedVersion=$(getConfigParamFromFile "version")
     local isVersionEqual=$(compareVersion "$installedVersion" "$ESPOCRM_VERSION" ">=")
 
     if [ -n "$isVersionEqual" ]; then
@@ -76,7 +219,11 @@ actionUpgrade() {
     fi
 
     echo >&2 "Start upgrading process from version $installedVersion."
-    runUpgradeStep
+
+    runUpgradeStep || {
+        return
+    }
+
     actionUpgrade
 }
 
@@ -158,43 +305,6 @@ runInstallationStep() {
         echo >&2 "$result"
         exit 1
     fi
-}
-
-# Bool: saveConfigParam "jobRunInParallel" "true"
-# String: saveConfigParam "language" "'en_US'"
-saveConfigParam() {
-    local name="$1"
-    local value="$2"
-
-    php -r "
-        require_once('$DOCUMENT_ROOT/bootstrap.php');
-
-        \$app = new \Espo\Core\Application();
-        \$config = \$app->getContainer()->get('config');
-
-        if (\$config->get('$name') !== $value) {
-            \$config->set('$name', $value);
-            \$config->save();
-        }
-    "
-}
-
-applyEnvironments() {
-    declare -A configParams=(
-        ['webSocketZeroMQSubmissionDsn']='ESPOCRM_ENV_WEBSOCKET_SUBMISSION_DSN'
-        ['webSocketZeroMQSubscriberDsn']='ESPOCRM_ENV_WEBSOCKET_SUBSCRIBER_DSN'
-        ['webSocketUrl']='ESPOCRM_ENV_WEB_SOCKET_URL'
-        ['siteUrl']='ESPOCRM_ENV_SITE_URL'
-    )
-
-    for paramName in "${!configParams[@]}"
-    do
-        local envName="${configParams[$paramName]}"
-
-        if [ -n "${!envName-}" ]; then
-            saveConfigParam "$paramName" "'${!envName}'"
-        fi
-    done
 }
 
 # ------------------------- START -------------------------------------
@@ -279,7 +389,7 @@ case $installationType in
         ;;
 esac
 
-applyEnvironments
+applyConfigEnvironments
 # ------------------------- END -------------------------------------
 
 exec "$@"
--- fpm/docker-websocket.sh
+++ fpm/docker-websocket.sh
@@ -4,7 +4,57 @@ set -eu
 
 DOCUMENT_ROOT="/var/www/html"
 
-# see docker-entrypoint.sh
+# entrypoint-utils.sh
+configPrefix="ESPOCRM_CONFIG_"
+
+compareVersion() {
+    local version1="$1"
+    local version2="$2"
+    local operator="$3"
+
+    echo $(php -r "echo version_compare('$version1', '$version2', '$operator');")
+}
+
+join() {
+    local sep="$1"; shift
+    local out; printf -v out "${sep//%/%%}%s" "$@"
+    echo "${out#$sep}"
+}
+
+getConfigParamFromFile() {
+    local name="$1"
+
+    php -r "
+        if (file_exists('$DOCUMENT_ROOT/data/config-internal.php')) {
+            \$config=include('$DOCUMENT_ROOT/data/config-internal.php');
+
+            if (array_key_exists('$name', \$config)) {
+                die(\$config['$name']);
+            }
+        }
+
+        \$config=include('$DOCUMENT_ROOT/data/config.php');
+        if (array_key_exists('$name', \$config)) {
+            die(\$config['$name']);
+        }
+    "
+}
+
+getConfigParam() {
+    local name="$1"
+
+    php -r "
+        require_once('$DOCUMENT_ROOT/bootstrap.php');
+
+        \$app = new \Espo\Core\Application();
+        \$config = \$app->getContainer()->get('config');
+
+        echo \$config->get('$name');
+    "
+}
+
+# Bool: saveConfigParam "jobRunInParallel" "true"
+# String: saveConfigParam "language" "'en_US'"
 saveConfigParam() {
     local name="$1"
     local value="$2"
@@ -22,26 +72,96 @@ saveConfigParam() {
     "
 }
 
-# see docker-entrypoint.sh
-applyEnvironments() {
-    declare -A configParams=(
-        ['webSocketZeroMQSubmissionDsn']='ESPOCRM_ENV_WEBSOCKET_SUBMISSION_DSN'
-        ['webSocketZeroMQSubscriberDsn']='ESPOCRM_ENV_WEBSOCKET_SUBSCRIBER_DSN'
-        ['webSocketUrl']='ESPOCRM_ENV_WEB_SOCKET_URL'
-        ['siteUrl']='ESPOCRM_ENV_SITE_URL'
-    )
+applyConfigEnvironments() {
+    local envName
+    local envValue
+    local configParamName
+    local configParamValue
 
-    for paramName in "${!configParams[@]}"
-    do
-        local envName="${configParams[$paramName]}"
+    compgen -v | while read -r envName; do
 
-        if [ -n "${!envName-}" ]; then
-            saveConfigParam "$paramName" "'${!envName}'"
+        if [[ $envName == "$configPrefix"* ]]; then
+
+            envValue="${!envName}"
+
+            configParamName=$(normalizeConfigParamName "$envName")
+            configParamValue=$(normalizeConfigParamValue "$envValue")
+
+            saveConfigParam "$configParamName" "$configParamValue"
+
         fi
+
     done
 }
 
-applyEnvironments
+isQuoteValue() {
+    local value="$1"
+
+    php -r "
+        echo isQuote('$value');
+
+        function isQuote (\$value) {
+            \$value = trim(\$value);
+
+            if (\$value === '0') {
+                return false;
+            }
+
+            if (empty(\$value)) {
+                return true;
+            }
+
+            if (!preg_match('/[^0-9.]+/', \$value)) {
+                return false;
+            }
+
+            if (in_array(\$value, ['null', '1'], true)) {
+                return false;
+            }
+
+            if (in_array(\$value, ['true', 'false'])) {
+                return false;
+            }
+
+            return true;
+        }
+    "
+}
+
+normalizeConfigParamName() {
+    local value="$1"
+
+    php -r "
+        \$value = str_ireplace('$configPrefix', '', '$value');
+        \$value = strtolower(\$value);
+
+        \$value = preg_replace_callback(
+            '/_([a-zA-Z])/',
+            function (\$matches) {
+                return strtoupper(\$matches[1]);
+            },
+            \$value
+        );
+
+        echo \$value;
+    "
+}
+
+normalizeConfigParamValue() {
+    local value=${1//\'/\\\'}
+
+    local isQuoteValue=$(isQuoteValue "$value")
+
+    if [ -n "$isQuoteValue" ] && [ "$isQuoteValue" = 1 ]; then
+        echo "'$value'"
+        return
+    fi
+
+    echo "$value"
+}
+# END: entrypoint-utils.sh
+
+applyConfigEnvironments
 
 /usr/local/bin/php /var/www/html/websocket.php
 
