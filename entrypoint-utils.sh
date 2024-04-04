355445115ba5dd3eb97779abab9625e5db1d81b2
Taras Machyshyn <taras@espocrm.com>
2024-04-04 10:22:45 +0300

    Define array values in config

--- entrypoint-utils.sh
+++ entrypoint-utils.sh
@@ -1,5 +1,10 @@
 configPrefix="ESPOCRM_CONFIG_"
 
+declare -A configPrefixArrayList=(
+    [logger]="ESPOCRM_CONFIG_LOGGER_"
+    [database]="ESPOCRM_CONFIG_DATABASE_"
+)
+
 compareVersion() {
     local version1="$1"
     local version2="$2"
@@ -61,13 +66,46 @@ saveConfigParam() {
         \$app = new \Espo\Core\Application();
         \$config = \$app->getContainer()->get('config');
 
-        if (\$config->get('$name') !== $value) {
-            \$injectableFactory = \$app->getContainer()->get('injectableFactory');
-            \$configWriter = \$injectableFactory->create('\\Espo\\Core\\Utils\\Config\\ConfigWriter');
-
-            \$configWriter->set('$name', $value);
-            \$configWriter->save();
+        if (\$config->get('$name') === $value) {
+            return;
         }
+
+        \$injectableFactory = \$app->getContainer()->get('injectableFactory');
+        \$configWriter = \$injectableFactory->create('\\Espo\\Core\\Utils\\Config\\ConfigWriter');
+
+        \$configWriter->set('$name', $value);
+        \$configWriter->save();
+    "
+}
+
+saveConfigArrayParam() {
+    local key1="$1"
+    local key2="$2"
+    local value="$3"
+
+    php -r "
+        require_once('$DOCUMENT_ROOT/bootstrap.php');
+
+        \$app = new \Espo\Core\Application();
+        \$config = \$app->getContainer()->get('config');
+
+        \$arrayValue = \$config->get('$key1') ?? [];
+
+        if (!is_array(\$arrayValue)) {
+            return;
+        }
+
+        if (array_key_exists('$key2', \$arrayValue) && \$arrayValue['$key2'] === $value) {
+            return;
+        }
+
+        \$injectableFactory = \$app->getContainer()->get('injectableFactory');
+        \$configWriter = \$injectableFactory->create('\\Espo\\Core\\Utils\\Config\\ConfigWriter');
+
+        \$arrayValue['$key2'] = $value;
+
+        \$configWriter->set('$key1', \$arrayValue);
+        \$configWriter->save();
     "
 }
 
@@ -136,16 +174,18 @@ applyConfigEnvironments() {
 
     compgen -v | while read -r envName; do
 
-        if [[ $envName == "$configPrefix"* ]]; then
-
-            envValue="${!envName}"
-
-            configParamName=$(normalizeConfigParamName "$envName")
-            configParamValue=$(normalizeConfigParamValue "$envValue")
-
-            saveConfigParam "$configParamName" "$configParamValue"
-
+        if [[ $envName != "$configPrefix"* ]]; then
+            continue
         fi
+
+        envValue="${!envName}"
+
+        if isConfigArrayParam "$envName" ; then
+            saveConfigArrayValue "$envName" "$envValue"
+            continue
+        fi
+
+        saveConfigValue "$envName" "$envValue"
 
     done
 }
@@ -190,9 +230,10 @@ isQuoteValue() {
 
 normalizeConfigParamName() {
     local value="$1"
+    local prefix=${2:-"$configPrefix"}
 
     php -r "
-        \$value = str_ireplace('$configPrefix', '', '$value');
+        \$value = str_ireplace('$prefix', '', '$value');
         \$value = strtolower(\$value);
 
         \$value = preg_replace_callback(
@@ -218,4 +259,50 @@ normalizeConfigParamValue() {
     fi
 
     echo "$value"
+}
+
+isConfigArrayParam() {
+    local envName="$1"
+
+    for i in "${!configPrefixArrayList[@]}"
+    do
+        if [[ "$envName" != ${configPrefixArrayList[$i]}* ]]; then
+            continue
+        fi
+
+        return 0 #true
+    done
+
+    return 1 #false
+}
+
+saveConfigValue() {
+    local envName="$1"
+    local envValue="$2"
+
+    local key=$(normalizeConfigParamName "$envName")
+    local value=$(normalizeConfigParamValue "$envValue")
+
+    saveConfigParam "$key" "$value"
+}
+
+saveConfigArrayValue() {
+    local envName="$1"
+    local envValue="$2"
+
+    for i in "${!configPrefixArrayList[@]}"
+    do
+        if [[ "$envName" != ${configPrefixArrayList[$i]}* ]]; then
+            continue
+        fi
+
+        local key1="$i"
+        local key2=$(normalizeConfigParamName "$envName" "${configPrefixArrayList[$i]}")
+
+        break
+    done
+
+    local value=$(normalizeConfigParamValue "$envValue")
+
+    saveConfigArrayParam "$key1" "$key2" "$value"
 }
